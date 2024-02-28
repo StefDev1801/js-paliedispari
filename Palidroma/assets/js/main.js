b648254d9629bee9e7fab2fa5bb320f806ea4e57
StefDev1801 <stefano200118@gmail.com>
2024-02-28 10:11:35 +0100

    add function to join arrays

--- Palidroma/assets/js/main.js
+++ Palidroma/assets/js/main.js
@@ -1,7 +1,56 @@
 //console.log('ou');
 //- Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+/** 
 
-const userWord = 'bob'
+
+console.log(userWord.split(''));
+const splitWord = userWord.split('')
+console.log(splitWord.reverse());//array rovesciato
+const reverserWOrdArray = splitWord.reverse()
+
+const reverserWOrd = reverserWOrdArray.join('')
+
+**
+ * check if a given word is a palindrom
+ * @param {string} userWord The word to ccheck
+ * @return {string}
+ *
+function isPalindrom(userWord) {
+    const reverserWOrd = userWord.split('').reverse().join('')
+
+    if (reverserWOrd == userWord) {
+        return true
+    }
+    return false
+}
+
+
+if (isPalindrom(userWord2)) {
+    console.log('is a palindrom');
+    
+}
+
+*/
+
+
+
+//FUNCTION TO REVERSE ARRAY
+
+
+
+
+
+
+
+
+
+
+
+
+const userWord = prompt('inserisci una parola ti dirò se è palindroma')
+
+
+
 console.log(userWord);
 let firsLetter;
 let lastLetter;
@@ -9,7 +58,7 @@ const parolaDritta = []
 const parolaRovescio = []
 let palindromeCounter = 0;
 
-
+    
     for (let index = 0; index < userWord.length; index++) {
         firsLetter = userWord[index];
         // console.log(firsLetter);
@@ -23,16 +72,33 @@ let palindromeCounter = 0;
         }
     console.log(parolaDritta);
     console.log(parolaRovescio);
-    if (parolaDritta === parolaRovescio) {
-        console.log('la tua parola è palindroma');
-    }else{
-        console.log('la tua parola non è palindroma');
+
+
+    //FUNCTION ADDED WITH THE HELP OF FABIO
+    function joinArrayAsString(arrayMadeOfOneWordSplitted) {
+        let joinWord = ''
+        for (let index = 0; index < arrayMadeOfOneWordSplitted.length; index++) {
+            let letter = arrayMadeOfOneWordSplitted[index];
+            joinWord += letter
+            
+        }
+        return joinWord;
     }
-/*if (palindromeCounter == userWord.length) {
+
+    
+    console.log(joinArrayAsString(parolaDritta));
+    console.log(joinArrayAsString(parolaRovescio));
+    
+
+if (joinArrayAsString(parolaDritta) == joinArrayAsString(parolaRovescio)) {
     console.log('The word IS palindrome');
 }else{
     console.log('The word IS NOT palindrome');
-}*/
+}
+
+ 
+
+/**/
     
 //questo array dovrò vedere quanto è lungo (es 4)
 //faccio lunghezza tot e lo confronto con 0, tot -1, tot-2
